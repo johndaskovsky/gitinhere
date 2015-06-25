612e364b1ee8eb409e3870f9adcd0b8917937d34
Will Mainwaring <willm@yogainternational.com>
2015-06-25 10:48:00 -0400

    add my awesome.js

---
+++ awesome.js
@@ -0,0 +1,10 @@
+var awesome = "Awesome!";
+
+var awesomeArray = ["Awesome", "Awesome", "Awesome", "Awesome"];
+
+var awesomeObject = {
+	awesome: "awesome";
+	awesome?: "awesome";
+	Awwwwson: "awesome";
+
+}
